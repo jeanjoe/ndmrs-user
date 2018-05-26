50075603cdda17a4bbd201ef7445acaca67d2d64
Jean Joe Irascible <manzede@gmail.com>
2018-05-26 11:28:05 +0300

    added stock books

---
+++ database/migrations/2018_05_26_072355_create_stock_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('health_facility_id');
+            $table->foreign('health_facility_id')->references('id')->on('health_facilities');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name', 191);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_books');
+    }
+}
